e8b97fc83032014a7b0632c91b240dd6d86d6ee6
Muhammad Arsalan <arsalan@Muhammads-MacBook-Pro-2.local>
2020-04-02 23:14:15 +0500

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+// Base Imports
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// React Material Design Imports
+
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+// React MDL : NAVBAR
+
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
+
+// NAVBAR END
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+// Base Imports
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// React Material Design Imports
+
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js'
 
 ReactDOM.render(
   <React.StrictMode>
